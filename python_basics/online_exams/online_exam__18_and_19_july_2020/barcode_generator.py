7e8eaf678364587c3d40f354de268f5111f0c1a7
SashPeshev <106158536+SashPeshev@users.noreply.github.com>
2022-05-25 09:33:25 +0300

    File folder

--- python_basics/online_exams/online_exam__18_and_19_july_2020/barcode_generator.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input())
-num2 = int(input())
-
-for i in range(int(num1 / 1000), int(num2 / 1000) + 1):
-    for j in range(int(num1 / 100) % 10, int(num2 / 100) % 10 + 1):
-        for k in range(int(num1 / 10) % 10, int(num2 / 10) % 10 + 1):
-            for m in range(num1 % 10, num2 % 10 + 1):
-                if i % 2 != 0 and j % 2 != 0 and k % 2 != 0 and m % 2 != 0:
-                    print(f"{i}{j}{k}{m} ", end="")
